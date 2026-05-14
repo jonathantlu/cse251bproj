181efe56e5335bb110213d79100ad0c1d9ef7a38
Jonathan Lu <jonathan.ty.lu@gmail.com>
2026-05-14 11:17:01 -0700

    change ropeablate.py

--- ropeablate.py
+++ ropeablate.py
@@ -16,6 +16,8 @@ class Rotary(torch.nn.Module):
     def __init__(self, dim, base=10000):
         super().__init__()
         self.inv_freq = 1.0 / (base ** (torch.arange(0, dim, 2).float() / dim))
+        self.dim = dim
+        self.base = base
         self.seq_len_cached = None
         self.cos_cached = None
         self.sin_cached = None
@@ -24,10 +26,11 @@ class Rotary(torch.nn.Module):
         seq_len = x.shape[1]
         if seq_len != self.seq_len_cached:
             self.seq_len_cached = seq_len
+            inv_freq = 1.0 / (self.base ** (torch.arange(0, self.dim, 2, device=x.device).float() / self.dim))
             t = torch.arange(seq_len, device=x.device).type_as(self.inv_freq)
             freqs = torch.outer(t, self.inv_freq).to(x.device)
-            self.cos_cached = freqs.cos().bfloat16()
-            self.sin_cached = freqs.sin().bfloat16()
+            self.cos_cached = freqs.cos().to(dtype=x.dtype)
+            self.sin_cached = freqs.sin().to(dtype=x.dtype)
         return self.cos_cached[None, :, None, :], self.sin_cached[None, :, None, :]
 
 def apply_rotary_emb(x, cos, sin):
